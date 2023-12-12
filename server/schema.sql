b4c0a57921bd4aab094350a072100f2065a87f13
Jose Felix <jmfelix195@gmail.com>
2023-12-12 10:29:58 -0700

    Implement db schema for postgres

---
+++ server/schema.sql
@@ -0,0 +1,44 @@
+CREATE TABLE products (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100) NOT NULL,
+  slogan VARCHAR(255),
+  description VARCHAR(500),
+  category VARCHAR(50),
+  default_price INTEGER
+);
+
+CREATE TABLE features (
+  id SERIAL PRIMARY KEY,
+  product_id INTEGER NOT NULL REFERENCES products(id),
+  name VARCHAR(100) NOT NULL,
+  value VARCHAR(100)
+);
+
+CREATE TABLE styles (
+  id SERIAL PRIMARY KEY,
+  product_id INTEGER NOT NULL REFERENCES products(id),
+  name VARCHAR(100) NOT NULL,
+  sale_price INTEGER,
+  original_price INTEGER,
+  is_default BOOLEAN
+);
+
+CREATE TABLE photos (
+  id SERIAL PRIMARY KEY,
+  style_id INTEGER NOT NULL REFERENCES styles(id),
+  url VARCHAR(255),
+  thumbnail_url VARCHAR(255)
+);
+
+CREATE TABLE skus (
+  id SERIAL PRIMARY KEY,
+  style_id INTEGER NOT NULL REFERENCES styles(id),
+  size VARCHAR(50),
+  quantity INTEGER
+);
+
+CREATE TABLE related_products (
+  id SERIAL PRIMARY KEY,
+  current_product_id INTEGER NOT NULL REFERENCES products(id),
+  related_product_id INTEGER NOT NULL REFERENCES products(id)
+);
