23b023354194e6c0afbcd70c3d968bec6a47b1d5
Jose Felix <jmfelix195@gmail.com>
2023-12-16 08:52:48 -0700

    Add loader.io verification token

--- server/routes.js
+++ server/routes.js
@@ -10,4 +10,10 @@ router.get('/products/:id/stylesNew', controller.products.getStylesNew);
 
 router.get('/products/:id/related', controller.products.getRelated);
 
+router.get('/loaderio-ba6d9bc647c859d4f7f60716bf7ae8d0', (req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Disposition', 'attachment; filename=token.txt');
+  res.end('loaderio-ba6d9bc647c859d4f7f60716bf7ae8d0');
+});
+
 module.exports = router;
